6145af1a546834263dfdee250ad287f3e0a4c441
riju951 <rijans.bhagat003@gmail.com>
2024-03-21 11:26:05 +0530

    Adding of files

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://rijansbhagat003:fQuUj3traDklPLe9@bookstoreproject.ubp9b3w.mongodb.net/?retryWrites=true&w=majority&appName=Bookstoreproject';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
